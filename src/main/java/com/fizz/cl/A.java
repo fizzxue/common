8b161118b1c011a51e7653257c9a5d245a106bd7
fizz <393193646@qq.com>
2023-05-28 14:52:59 +0800

    cl

--- src/main/java/com/fizz/cl/A.java
+++ src/main/java/com/fizz/cl/A.java
@@ -2,8 +2,8 @@ package com.fizz.cl;
 
 public class A {
     public void a() {
-        System.out.println(B.class.getClassLoader());
-        System.out.println(new B().getClass().getClassLoader());
+        System.out.println(App.class.getClassLoader());
+        System.out.println(new App().getClass().getClassLoader());
         System.out.println("1111111111111111");
     }
 }
